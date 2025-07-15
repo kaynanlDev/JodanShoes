14502c2992604ceaaa441b813248acd464f5c737
kaynanDev <kaynan2817@gmail.com>
2025-07-15 14:11:47 -0300

    Adição do tailwindcss e criação do header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <header class="py-2.5">
+    <p class="text-[#121214] text-base text-center font-1">
+      Frete grátis para todo o Brasil
+    </p>
+  </header>
+</template>
 
 <style scoped></style>
